3e33974dfbdf58550a2c426ff2cad1298aa43c54
hetenyiboraszat <panczelkaroly79@gmail.com>
2026-09-24 10:00:21 +0200

    active button design

--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
 import { programs } from './data/data'
 import './style.css'
-import { renderCateg, renderprograms } from './ui/render'
+import { renderCateg, renderprograms, renderStat } from './ui/render'
+import { createIcons, icons } from 'lucide';
+
+createIcons({ icons });
 
 renderprograms(programs)
 renderCateg(programs)
+renderStat(programs)
 
 window.onCategClick= function onCategClick(e) {
+  document.querySelectorAll("button").forEach(button =>button.classList.remove('activeBtn'))
+  if(e.target.tagName == 'BUTTON') e.target.classList.add('activeBtn')
+  else return
   const selectedCateg = e.target.textContent
   const filteredPrograms = programs.filter(obj => obj.category == selectedCateg)
   selectedCateg=='összes' ? renderprograms(programs) :renderprograms(filteredPrograms)
 }
+
+window.availablePrograms = function availablePrograms() {
+  document.querySelectorAll("button").forEach(button =>button.classList.remove('activeBtn'))
+  document.querySelector('.btn').classList.add('activeBtn')
+  const filteredPrograms=programs.filter(obj=>obj.capacity>obj.participants)
+  renderprograms(filteredPrograms)
+  
+}
+
--- src/ui/render.js
+++ src/ui/render.js
@@ -1,3 +1,5 @@
+import { programs } from "../data/data";
+
 export function renderprograms(arr) {
     const cardStr = arr.map(obj=>
         `
@@ -20,11 +22,31 @@ export function renderCateg(arr) {
     let categories = arr.map(obj=>obj.category)
     categories = [...new Set(categories)]
     console.log(categories);
-    categories = [... categories,"összes"]
+    //categories = [... categories,"összes"]
 
     const btnStr =categories.map(ctg=> `
         <button>${ctg}</button>
         `).join('')
-        document.querySelector('header').innerHTML=btnStr
+        document.querySelector('header').innerHTML=btnStr+'<button class="activeBtn">összes</button>'
+}
+
+export function renderStat(arr) {
+    let sumParticipants =  programs.reduce((sum, obj)=>sum+obj.participants  , 0)
+    let avgPrice = programs.reduce((sum,obj) => sum+obj.price/8, 0)
+    let currentRevenue = programs.reduce((sum,obj)=>sum+obj.price*obj.participants,0)
+    let sumIndoor = programs.reduce((sum,obj) => obj.indoor? sum+1:sum , 0 )
+    let sumOutdoor = programs.reduce((sum,obj) => obj.indoor? sum:sum +1 , 0 )
+    
+    document.querySelector("footer").innerHTML=`
+    <p>Összes résztvevő: ${sumParticipants} fő</p>
+    <p>Átlagos részvételi díj: ${avgPrice} Ft</p>
+    <p>Jelentlegi bevétel: ${currentRevenue} Ft</p>
+    <p>Beltéri programok: ${sumIndoor}</p>
+    <p>Kültéri programok: ${sumOutdoor}</p>
+    `
+    
+    
+    
+    
     
 }
